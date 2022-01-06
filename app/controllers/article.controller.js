0e9faa4eafdd6cb7d17595aa94cce247948ba7f6
Rafi Priatna K <rafipriatnak@gmail.com>
2022-01-06 17:51:32 +0700

    Update cache

--- app/controllers/article.controller.js
+++ app/controllers/article.controller.js
@@ -1,6 +1,8 @@
 const Article = require("../models/article.model")
+const NodeCache = require("node-cache")
+const cache = new NodeCache()
 
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
     if (Object.keys(req.body).length === 0) return res.status(400).json({ message: "Article cannot be empty!" })
 
     const values = new Article({
@@ -9,10 +11,15 @@ exports.create = (req, res) => {
         body: req.body.body,
     })
 
-    Article.create(values, (err, data, msg) => {
+    Article.create(values, async (err, data) => {
         if (err) return res.status(500).json({ message: err.message || "Internal Server Error." })
 
-        return res.json({ message: msg, data })
+        Article.getAll(async (err, dataArticles) => {
+            if (err) return res.status(500).json({ message: err.message || "Internal Server Error." })
+            let articles = fetchData(dataArticles)
+            await cache.set("articles", articles, 100000)
+            return res.json({ message: "Article created.", data })
+        })
     })
 }
 
@@ -20,10 +27,88 @@ exports.getAll = (req, res) => {
     const qry = req.query.query
     const author = req.query.author
     const page = req.params.page < 0 ? 1 : parseInt(req.params.page) || 1
+    let cached = cache.get("articles")
 
-    Article.getAll(qry, author, page, (err, data, msg) => {
-        if (err) return res.status(500).json({ message: err.message || "Internal Server Error." })
+    if (cached) {
+        let articles = cached
+        totalArticles = articles.length
+        return res.json({ message: "List of all articles from cache.", data: response(articles, totalArticles, page, qry, author) })
+    } else {
+        Article.getAll((err, dataArticles) => {
+            if (err) return res.status(500).json({ message: err.message || "Internal Server Error." })
 
-        return res.json({ message: msg, data })
+            let articles = fetchData(dataArticles)
+            totalArticles = articles.length
+
+            cache.set("articles", articles, 100000)
+
+            return res.json({ message: "List of all articles.", data: response(articles, totalArticles, page, qry, author) })
+        })
+    }
+}
+
+function fetchData(data) {
+    let articles = []
+    data.map(item => {
+        articles.push({
+            author: item.author,
+            title: item.title,
+            body: item.body,
+            created_at: item.created_at,
+        })
     })
+    return articles
+}
+
+function paginationResponse(totalArticles, limit, currentPage) {
+    const url = "http://localhost:8000/api/articles/"
+    const totalPages = Math.ceil(totalArticles / limit)
+    const current = url + currentPage
+    const previous = (currentPage - 1) < 1 ? null : url + (currentPage - 1)
+    const next = (currentPage + 1) > totalPages ? null : url + (currentPage + 1)
+
+    return {
+        total: totalPages,
+        current,
+        previous,
+        next
+    }
+}
+
+function paginate(article, size, current) {
+    return article.slice((current - 1) * size, current * size)
+}
+
+function filter(articles, query, author) {
+    if (query) {
+        let filtered = articles.filter(item => item.title.includes(query) || item.body.includes(query))
+
+        if (author) {
+            return filtered.filter(item => item.author.includes(author))
+        }
+
+        return filtered
+    }
+
+    if (author) {
+        return articles.filter(item => item.author.includes(author))
+    }
+}
+
+function response(articles, totalArticles, page, qry, author) {
+    const limit = 10
+    const pagination = paginationResponse(totalArticles, limit, page)
+
+    if (qry || author) {
+        let tempArticle = filter(articles, qry, author)
+        totalArticles = tempArticle.length
+        articles = paginate(tempArticle, limit, page)
+    } else {
+        articles = paginate(articles, limit, page)
+    }
+    return {
+        pagination,
+        total_articles: totalArticles,
+        article: articles
+    }
 }
--- app/models/article.model.js
+++ app/models/article.model.js
@@ -1,5 +1,3 @@
-const NodeCache = require("node-cache");
-const myCache = new NodeCache();
 const sql = require("../utils/mysql")
 
 const Article = function (data) {
@@ -15,103 +13,18 @@ Article.create = (data, result) => {
             id: res.insertId,
             ...data
         }
-        myCache.del("articles");
-        return result(null, articleData, "Article created.")
+        return result(null, articleData)
     })
 }
 
-Article.getAll = async (qry, author, page, result) => {
-    const limit = 10
-    let value = myCache.get("articles");
-    let totalArticles = 0
+Article.getAll = async (result) => {
+    let query = "SELECT name as author, title, body, created_at FROM articles INNER JOIN authors ON articles.author_id = authors.id ORDER BY created_at DESC"
 
-    if (value != undefined) {
-        let articles = value
-        totalArticles = articles.length
-        if (qry || author) {
-            let tempArticle = filter(articles, qry, author)
-            totalArticles = tempArticle.length
-            articles = paginate(tempArticle, limit, page)
-        } else {
-            articles = paginate(articles, limit, page)
-        }
-        const pagination = paginationResponse(totalArticles, limit, page)
+    sql.query(query, (err, res) => {
+        if (err) return result(err, null)
 
-        return result(null, response(articles, pagination), "List of all articles from cache.")
-    } else {
-        let query = "SELECT name as author, title, body, created_at FROM articles INNER JOIN authors ON articles.author_id = authors.id ORDER BY created_at DESC"
-
-        sql.query(query, (err, res) => {
-            if (err) return result(err, null)
-
-            totalArticles = res.length
-            let articles = []
-            res.map(item => {
-                articles.push({
-                    author: item.author,
-                    title: item.title,
-                    body: item.body,
-                    created_at: item.created_at,
-                })
-            })
-
-            myCache.set("articles", articles, 10000);
-
-            if (qry || author) {
-                let tempArticle = filter(articles, qry, author)
-                totalArticles = tempArticle.length
-                articles = paginate(tempArticle, limit, page)
-            } else {
-                articles = paginate(articles, limit, page)
-            }
-            const pagination = paginationResponse(totalArticles, limit, page)
-
-            return result(null, response(articles, pagination), "List of all articles.")
-        })
-    }
-}
-
-function paginationResponse(totalArticles, limit, currentPage) {
-    const url = "http://localhost:8000/api/articles/"
-    const totalPages = Math.ceil(totalArticles / limit)
-    const current = url + currentPage
-    const previous = (currentPage - 1) < 1 ? null : url + (currentPage - 1)
-    const next = (currentPage + 1) > totalPages ? null : url + (currentPage + 1)
-
-    return {
-        total: totalPages,
-        current,
-        previous,
-        next
-    }
-}
-
-function paginate(article, size, current) {
-    return article.slice((current - 1) * size, current * size);
-}
-
-function filter(articles, query, author) {
-    if (query) {
-        let filtered = articles.filter(item => item.title.includes(query) || item.body.includes(query))
-        
-        if (author) {
-            return filtered.filter(item => item.author.includes(author))
-        }
-
-        return filtered
-    }
-
-    if (author) {
-        return articles.filter(item => item.author.includes(author))
-    }
-}
-
-function response(article, pagination) {
-    return {
-        pagination,
-        total_articles: article.length,
-        article: article
-    }
+        return result(null, res)
+    })
 }
 
 module.exports = Article
